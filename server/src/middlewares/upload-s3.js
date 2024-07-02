0f59f2ca7a120099062005bd6bdccaf8ae6aabb6
Reginald Bartiana <reginadblog123@gmail.com>
2024-07-02 13:20:04 +0800

    refactor

--- server/src/middlewares/upload-s3.js
+++ server/src/middlewares/upload-s3.js
@@ -3,3 +3,20 @@ import AWS from "aws-sdk";
 AWS.config.update({ region: process.env.AWS_S3});
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01'});
+
+module.exports = async (fileStream, fileName) => {
+    try {
+        let uploadParams = {
+            Bucket: process.env.AWS_S3,
+            Key: fileName,
+            Body: fileStream
+        };
+
+        const data = await s3.upload(uploadParams).promise();
+        return data
+    } catch(error){
+        console.log('Error uploading to S3', error);
+
+        throw error;
+    } 
+}
